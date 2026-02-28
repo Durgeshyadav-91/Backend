7ee685a7cea01cd44ad4952083d410b3d69bc309
Durgesh <yadavdurgesh8160@gmail.com>
2026-02-28 18:18:21 +0530

    database code change

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 // require("dotenv").config({path: "./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
 dotenv.config({ path: "./env" })
 
-
-
 connectDB();
-
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-
-//         app.on("Error", (error) => {
-//             console.log("Error ", error);
-//             throw error;
-//         });
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is Running on Port: ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.error("ERROR: ", error);
-//         throw error
-//     }
-// })()
