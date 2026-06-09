0337977b18bef3285b401d364efa54b404dcac10
Durgesh <yadavdurgesh8160@gmail.com>
2026-06-09 13:10:24 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -212,4 +212,29 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+    const user = await User.findById(req.user?.id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid password")
+    }
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(200, req.user, "Current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const {fullName, email} = req.body;
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails }
--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
+    export const DB_NAME = "durgesh"
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}s`)
         // console.log(` \n MondoDB Database Connected Successfully ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("Mongo DB Connection Failed: ", error);
